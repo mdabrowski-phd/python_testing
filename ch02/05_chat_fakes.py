bdcc0fa7ac319cf9ce65488fc6e1ff1f5a8adabc
mdabrowski-phd <micdab90@gmail.com>
2024-10-12 22:26:31 +0200

    fake server implementation, succesful connection test

---
+++ ch02/05_chat_fakes.py
@@ -0,0 +1,124 @@
+import unittest
+import unittest.mock
+from multiprocessing.managers import SyncManager, ListProxy
+
+
+class TestChatAcceptance(unittest.TestCase):
+    def test_message_exchange(self):
+        user1 = ChatClient("John Doe")
+        user2 = ChatClient("Harry Potter")
+        user1.send_message("Hello World!")
+        messages = user2.fetch_messages()
+        assert messages == ["John Doe: Hello World!"]
+
+
+class TestChatClient(unittest.TestCase):
+    def test_nickname(self):
+        client = ChatClient("User 1")
+        assert client.nickname == "User 1"
+
+    def test_send_messsage(self):
+        client = ChatClient("User 1")
+        client.connection = unittest.mock.Mock()
+        sent_message = client.send_message("Hello World!")
+        assert sent_message == "User 1: Hello World!"
+
+    def test_client_connection(self):
+        client = ChatClient("User 1")
+        connection_spy = unittest.mock.Mock()
+        with unittest.mock.patch.object(client, "_get_connection", return_value=connection_spy):
+            client.send_message("Hello World!")
+        connection_spy.broadcast.assert_called_with(("User 1: Hello World!"))
+
+
+class TestConnection(unittest.TestCase):
+    def test_broadcast(self):
+        with unittest.mock.patch.object(Connection, "connect"):
+            c = Connection(("localhost", 9090))
+        
+        with unittest.mock.patch.object(c, "get_messages", return_value=[]):
+            c.broadcast("test message")
+            assert c.get_messages()[-1] == "test message"
+
+    def test_exchange_with_server(self):
+        with unittest.mock.patch(
+            "multiprocessing.managers.listener_client",
+            new={"pickle": (None, FakeServer())}
+            ):
+                c1 = Connection(("localhost", 9090))
+                c2 = Connection(("localhost", 9090))
+
+                c1.broadcast("connected message")
+                assert c2.get_messages()[-1] == "connected message"
+        
+
+class ChatClient:
+    def __init__(self, nickname):
+        self.nickname = nickname
+        self._connection = None
+
+    def send_message(self, message):
+        sent_message = f"{self.nickname}: {message}"
+        self.connection.broadcast(sent_message)
+        return sent_message
+
+    @property
+    def connection(self):
+        if self._connection is None:
+            self._connection = self._get_connection()
+        return self._connection
+
+    @connection.setter
+    def connection(self, value):
+        if self._connection is not None:
+            self._connection.close()
+        self._connection = value
+
+    def _get_connection(self):
+        return Connection(("localhost", 9090))
+    
+
+class Connection(SyncManager):
+    def __init__(self, address):
+        self.register("get_messages", proxytype=ListProxy)
+        super().__init__(address=address, authkey=b'mychatsecret')
+        self.connect()
+
+    def broadcast(self, message):
+        messages = self.get_messages()
+        messages.append(message)
+
+
+class FakeServer:
+    def __init__(self):
+        self.last_command = None
+        self.last_args = None
+        self.messages = []
+
+    def __call__(self, *args, **kwargs):
+        return self
+
+    def send(self, data):
+        callid, command, args, kwargs = data
+        self.last_command = command
+        self.last_args = args
+
+    def recv(self, *args, **kwargs):
+        if self.last_command in ("dummy", "incref", "decref", "accept_connection"):
+            return "#RETURN", None
+        elif self.last_command == "create":
+            return "#RETURN", ("fakeid", tuple())
+        elif self.last_command == "append":
+            self.messages.append(self.last_args[0])
+            return "#RETURN", None
+        elif self.last_command == "__getitem__":
+            return "#RETURN", self.messages[self.last_args[0]]
+        else:
+            return "#ERROR", ValueError(f"{self.last_command} - {self.last_args}")
+
+    def close(self):
+        pass
+    
+
+if __name__ == '__main__':
+    unittest.main()
