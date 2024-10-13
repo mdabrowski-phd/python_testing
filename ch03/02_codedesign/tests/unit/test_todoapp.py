aaf2531614e9efe9496312066dfafddbde79974b
mdabrowski-phd <micdab90@gmail.com>
2024-10-13 21:40:49 +0200

    refactorization of todo app, cancel dbpath dependance

--- ch03/02_codedesign/tests/unit/test_todoapp.py
+++ ch03/02_codedesign/tests/unit/test_todoapp.py
@@ -7,24 +7,21 @@ from todo.app import TODOApp
 
 
 class TestTODOApp(unittest.TestCase):
-    def test_default_dbpath(self):
-        app = TODOApp()
-        assert Path('.').resolve() == Path(app._dbpath).resolve()
-
-    def test_accepts_dbpath(self):
-        expected_path = Path(tempfile.gettempdir(), "anything")
-        app = TODOApp(dbpath=str(expected_path))
-        assert expected_path == Path(app._dbpath)
+    def test_noloader(self):
+        app = TODOApp(
+            io=(Mock(return_value="quit"), Mock()),
+            dbmanager=None
+            )
+        app.run()
+        assert app._entries == []
 
     def test_load(self):
-        dbpath = Path(tempfile.gettempdir(), "anything")
         dbmanager = Mock(load=Mock(return_value=["buy milk", "buy water"]))
         app = TODOApp(
             io=(Mock(return_value="quit"), Mock()),
-            dbpath=dbpath,
             dbmanager=dbmanager
             )
         app.run()
 
-        dbmanager.load.assert_called_with(dbpath)
+        dbmanager.load.assert_called_with()
         assert app._entries == ["buy milk", "buy water"]
