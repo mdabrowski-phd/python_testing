0b3c22e3e89ec8d79da5741500d511c17e66449f
mdabrowski-phd <micdab90@gmail.com>
2024-10-14 19:42:39 +0200

    init pytest framework, CI with Travis to chat app

---
+++ ch05/test_capsys.py
@@ -0,0 +1,11 @@
+def myapp():
+    print("MyApp Started")
+
+
+def test_capsys(capsys):
+    myapp()
+
+    out, err = capsys.readouterr()
+    
+    assert out == "MyApp Started\n"
+    
---
+++ ch05/test_randomness.py
@@ -0,0 +1,5 @@
+def test_something(random_number_generator):
+    a = random_number_generator()
+    b = 10
+    assert a + b >= 10
+    
