457f6ce30fc4aa5a453717d7074ffb417f94e5fd
mdabrowski-phd <micdab90@gmail.com>
2024-10-14 13:06:17 +0200

    proper project structure, all tests passed

---
+++ ch04/tests/test_chat.py
@@ -0,0 +1,19 @@
+import unittest
+import unittest.mock
+
+from chat.client import ChatClient
+from chat.server import new_chat_server
+
+
+class TestChatAcceptance(unittest.TestCase):
+    def test_message_exchange(self):
+        with new_chat_server():
+            user1 = ChatClient("John Doe")
+            user2 = ChatClient("Harry Potter")
+            user1.send_message("Hello World!")
+            messages = user2.fetch_messages()
+            assert messages == ["John Doe: Hello World!"]
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ ch04/tests/test_connection.py
@@ -0,0 +1,30 @@
+import unittest
+import unittest.mock
+
+from chat.client import Connection
+from chat.server import FakeServer
+
+
+class TestConnection(unittest.TestCase):
+    def test_broadcast(self):
+        with unittest.mock.patch.object(Connection, "connect"):
+            c = Connection(("localhost", 9090))
+        
+        with unittest.mock.patch.object(c, "get_messages", return_value=[]):
+            c.broadcast("test message")
+            assert c.get_messages()[-1] == "test message"
+
+    def test_exchange_with_server(self):
+        with unittest.mock.patch(
+            "multiprocessing.managers.listener_client",
+            new={"pickle": (None, FakeServer())}
+            ):
+                c1 = Connection(("localhost", 9090))
+                c2 = Connection(("localhost", 9090))
+
+                c1.broadcast("connected message")
+                assert c2.get_messages()[-1] == "connected message"
+
+
+if __name__ == '__main__':
+    unittest.main()
