bbefb28909bbbb985ab40a01b4ead1d1c8c1fc63
mdabrowski-phd <micdab90@gmail.com>
2024-10-14 10:29:36 +0200

    error replication, mock object for input an output operations

--- ch03/03_regression/tests/test_regression.py
+++ ch03/03_regression/tests/test_regression.py
@@ -1,6 +1,5 @@
 import unittest
-import threading
-import queue
+from unittest.mock import Mock
 import tempfile
 from pathlib import Path
 
@@ -9,41 +8,21 @@ from todo.db import BasicDB
 
 
 class TestRegression(unittest.TestCase):
-    def setUp(self):
-        self.outputs = queue.Queue()
-        self.inputs = queue.Queue()
-        
-        self.fake_output = lambda txt: self.outputs.put(txt)
-        self.fake_input = lambda: self.inputs.get()
-
-        self.get_output = lambda: self.outputs.get(timeout=1)
-        self.send_input = lambda cmd: self.inputs.put(cmd)
-
-    def create_thread(self, dirname):
-        return threading.Thread(
-            target=TODOApp(
-                io=(self.fake_input, self.fake_output),
-                dbmanager=BasicDB(Path(dirname, "db"))
-                ).run,
-                daemon=True)
-
     def test_os_release(self):
         with tempfile.TemporaryDirectory() as tmpdirname:
-            app_thread = self.create_thread(tmpdirname)
-            app_thread.start()
-            self.get_output()
+            app = TODOApp(
+                io=(Mock(side_effect=[
+                    "add buy milk",
+                    'add install "Ubuntu"',
+                    "quit"
+                    ]), Mock()),
+                dbmanager=BasicDB(Path(tmpdirname, "db"))
+                )
+            app.run()
+
+            restarted_app = TODOApp(
+                io=(Mock(side_effect="quit"), Mock()),
+                dbmanager=BasicDB(Path(tmpdirname, "db"))
+                )
+            restarted_app.run()
             
-            self.send_input("add buy milk")
-            self.send_input('add install "Ubuntu"')
-            self.send_input("quit")
-            app_thread.join(timeout=1)
-
-            while True:
-                try:
-                    self.get_output()
-                except queue.Empty:
-                    break
-
-            app_thread = self.create_thread(tmpdirname)
-            app_thread.start()
-            self.get_output()
