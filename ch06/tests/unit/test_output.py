6270bbeebe6d85a0ea3f8408112d597502077a9c
mdabrowski-phd <micdab90@gmail.com>
2024-10-15 15:56:07 +0200

    pytest.mark to override kernel flag

--- ch06/tests/unit/test_output.py
+++ ch06/tests/unit/test_output.py
@@ -1,14 +1,30 @@
-from fizzbuzz import outfizz, outbuzz, endnum
+import pytest
+import fizzbuzz
 
 
-def test_outfizz(capsys):
-    outfizz()
+def test_output(expected_output, capsys):
+    func, expected = expected_output
 
-    out, _ = capsys.readouterr()
-    assert out == "fizz"
-
-def test_outbuzz(capsys):
-    outbuzz()
+    func()
 
     out, _ = capsys.readouterr()
-    assert out == "buzz"
+    assert out == expected
+
+
+@pytest.fixture(params=["fizz", "buzz"])
+def expected_output(request):
+    text = request.param
+    if request.config.getoption("--upper"):
+        text = text.upper()
+    yield getattr(fizzbuzz, f"out{request.param}"), text
+
+
+@pytest.mark.textcase("lower")
+def test_lowercase_output(expected_output, capsys):
+    func, expected = expected_output
+
+    func()
+
+    out, _ = capsys.readouterr()
+    assert out == expected
+    
