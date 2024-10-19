d23efdf81ad6f45e301396b20bbd021cc9f3baf0
mdabrowski-phd <micdab90@gmail.com>
2024-10-19 19:35:00 +0200

    mock implementation of server part

--- ch11/httpclient/tests/test_httpclient.py
+++ ch11/httpclient/tests/test_httpclient.py
@@ -1,18 +1,27 @@
 import json
 from httpclient import HTTPClient
+from requests_mock import Mocker
 
 
 class TestHTTPClient:
     def test_GET(self):
         client = HTTPClient(url="http://httpbin.org/get")
-        response = client.GET()
+
+        with Mocker() as m:
+            m.get(client._url, text='{"Host": "httpbin.org", "args": {}}')
+            response = client.GET()
 
         assert '"Host": "httpbin.org"' in response
         assert '"args": {}' in response
 
     def test_GET_params(self):
         client = HTTPClient(url="http://httpbin.org/get?alpha=1")
-        response = client.GET()
+        
+        with Mocker() as m:
+            m.get(client._url, text='''{"headers": {"Host": "httpbin.org"},
+                                        "args": {"alpha": "1"}}''')
+            response = client.GET()
+
         response = json.loads(response)
 
         assert response["headers"]["Host"] == "httpbin.org"
@@ -20,7 +29,13 @@ class TestHTTPClient:
 
     def test_POST(self):
         client = HTTPClient(url="http://httpbin.org/post?alpha=1")
-        response = client.POST(beta=2)
+        
+        with Mocker() as m:
+            m.post(client._url, json={"headers": {"Host": "httpbin.org"},
+                                      "args": {"alpha": "1"},
+                                      "form": {"beta": "2"}})
+            response = client.POST(beta=2)
+
         response = json.loads(response)
 
         assert response["headers"]["Host"] == "httpbin.org"
@@ -29,7 +44,11 @@ class TestHTTPClient:
 
     def test_DELETE(self):
         client = HTTPClient(url="http://httpbin.org/anything/27")
-        response = client.DELETE()
+        
+        with Mocker() as m:
+            m.delete(client._url, json={"method": "DELETE", 
+                                        "url": "http://httpbin.org/anything/27"})
+            response = client.DELETE()
 
         assert '"method": "DELETE"' in response
         assert '"url": "http://httpbin.org/anything/27"' in response
