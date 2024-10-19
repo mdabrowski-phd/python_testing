2b7ceade97ee63a2180f49990e79ed0d4a5b9135
mdabrowski-phd <micdab90@gmail.com>
2024-10-19 23:58:19 +0200

    full implementation of wsgi client in django framework, tests passed

--- ch11/webframeworks/src/wbtframeworks/django/__init__.py
+++ ch11/webframeworks/src/wbtframeworks/django/__init__.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import json
 from django.urls import re_path
 from django.conf import settings
 from django.http import HttpResponse
@@ -17,9 +18,28 @@ def home(request):
     return HttpResponse('Hello World')
 
 
+def get(request):
+    if request.META.get("SERVER_PORT") == "80":
+        host_no_default_port = request.META["HTTP_HOST"].replace(":80", "")
+        request.META["HTTP_HOST"] = host_no_default_port
+    host = request.META["HTTP_HOST"]
+
+    response = HttpResponse(json.dumps({
+        "method": request.META["REQUEST_METHOD"],
+        "headers": {"Host": host},
+        "args": {p: v for (p, v) in request.GET.items()},
+        "form": {p: v for (p, v) in request.POST.items()},
+        "url": request.build_absolute_uri()
+    }, sort_keys=True))
+    response['Content-Type'] = 'application/json'
+    return response
+
+
 urlpatterns = [
+    re_path(r'^get$', get),
+    re_path(r"^anything.*$", get),
     re_path(r'^$', home),
-    ]
+]
 
 
 def make_application():
