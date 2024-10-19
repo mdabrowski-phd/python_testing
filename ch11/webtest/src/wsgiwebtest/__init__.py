e7392d6922cbe7083bc063c12bd062d28c8b9d89
mdabrowski-phd <micdab90@gmail.com>
2024-10-19 22:15:24 +0200

    e2e testing of httpclient app using webtest framework

--- ch11/webtest/src/wsgiwebtest/__init__.py
+++ ch11/webtest/src/wsgiwebtest/__init__.py
@@ -1,7 +1,29 @@
+import json
+import urllib.parse
+
+
 class Application:
     def __call__(self, environ, start_response):
         start_response(
             '200 OK', 
-            [('Content-type', 'test/plain; charset=utf-8')]
+            [('Content-type', 'application/json; charset=utf-8')]
         )
-        return ["Hello World!".encode("utf-8")]
+
+        form_params = {}
+        if environ.get('CONTENT_TYPE') == 'application/x-www-form-urlencoded':
+            req_body = environ["wsgi.input"].read().decode("ascii")
+            form_params = {k: v for k, v in urllib.parse.parse_qsl(req_body)}
+
+        if environ.get("SERVER_PORT") == "80":
+            host = environ["SERVER_NAME"]
+        else:
+            host = environ["HTTP_HOST"]
+
+        return [json.dumps({
+            "method": environ["REQUEST_METHOD"],
+            "headers": {"Host": host},
+            "url": f"{environ['wsgi.url_scheme']}://{host}{environ['PATH_INFO']}",
+            "args": {k: v for k, v in urllib.parse.parse_qsl(environ["QUERY_STRING"])},
+            "form": form_params
+        }).encode("utf-8")]
+    
